510f2ac774a205be4dbe988ac9b84bb919966696
Remil Michael <mail@remil.me>
2022-06-09 19:42:16 -0400

    migrate project to another application name

--- src/main/java/me/remil/CAuthServerApplication.java
+++ src/main/java/me/remil/CAuthServerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TotpWalletApplication {
+public class CAuthServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TotpWalletApplication.class, args);
+		SpringApplication.run(CAuthServerApplication.class, args);
 	}
 
 }
--- src/main/java/me/remil/controller/AuthenticationController.java
+++ src/main/java/me/remil/controller/AuthenticationController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +14,8 @@ import me.remil.dto.CheckEmailExists;
 import me.remil.dto.UserDTO;
 import me.remil.service.UserService;
 
-@RestController("/authentication")
+@RequestMapping("/api/authentication")
+@RestController
 @CrossOrigin(origins = {"http://localhost:3000"})
 public class AuthenticationController {
 	
